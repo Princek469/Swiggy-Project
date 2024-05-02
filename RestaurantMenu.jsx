2f95e171abcec50fa80867c0c2bc6895cb788805
Princek469 <168093640+Princek469@users.noreply.github.com>
2024-05-03 02:32:38 +0530

    RestaurantMenu.jsx
    Restaurant Menu

--- RestaurantMenu.jsx
+++ RestaurantMenu.jsx
@@ -1,43 +1,56 @@
 import React from "react";
-import {useEffect , useState}  from "react";
+import { useState , useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
 
-function RestaurantMenu(){
+function RestaurantMenu() {
+  const [resMenu, setResMenu] = useState(null);
 
-    const [resMenu , setResMenu] = useState(null)
+  const { resId } = useParams();
 
-    useEffect(() => {
-        fetchMenu();
-    } , [])
+  useEffect(() => {
+    fetchMenu();
+  }, []);
 
+  
+async function fetchMenu(){
+  const response = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5476094&lng=77.4788014&restaurantId=${resId}&catalog_qa=undefined&isMenuUx4=true&submitAction=ENTER`)
+  const json = await response.json()
+  console.log(json)
 
-    async function fetchMenu() {
-        const response = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5476094&lng=77.4788014&restaurantId=683307&catalog_qa=undefined&isMenuUx4=true&submitAction=ENTER`)
-        const json = await response.json()
+  setResMenu(json?.data)
+}
 
-        console.log(json)
-        setResMenu(json?.data)
-
-    }
     
+  if (resMenu === null) return <Shimmer></Shimmer>;
+  const {name , cuisines , costForTwoMessage , avgRatingString} = resMenu?.cards[2]?.card?.card?.info;
 
-    return (resMenu === null) ? <Shimmer></Shimmer> : (
-        <div className="menu">
-            {/* we will use destructuring property there */}
-            <h1>{resMenu?.cards[2]?.card?.card?.info?.name}</h1>
-            <p>{resMenu?.cards[2]?.card?.card?.info?.cuisines.join(", ")}
-             - {resMenu?.cards[2]?.card?.card?.info?.costForTwoMessage}</p>
-            
-            <h3>{resMenu?.cards[2]?.card?.card?.info?.avgRatingString
-}⭐</h3>
+  const { itemCards } = resMenu?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
-            <ul>
-                <li>Biryani</li>
-                <li>Diet Coke</li>
-                <li>Burgers</li>
-            </ul>
-        </div>
-    )
+  console.log(itemCards)
+
+
+  return (
+    <div className="menu">
+      <h1>{name}</h1>
+      <p>
+        {cuisines} - {costForTwoMessage}
+      </p>
+      <h3>{avgRatingString}⭐</h3>
+
+      <ul className="restaurantMenuNames">
+        {
+          itemCards && itemCards.map((item) => {
+            return (
+              <li key={item?.card?.info?.id}>
+                {item?.card?.info?.name} - Rs.{item?.card?.info?.price/100}
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  );
 }
 
 export default RestaurantMenu;
