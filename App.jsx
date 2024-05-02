ef186fd9d3ce4102dc25f0395af46c462e18a5f2
Princek469 <168093640+Princek469@users.noreply.github.com>
2024-05-03 02:33:14 +0530

    App.jsx

--- App.jsx
+++ App.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body"
-import { createBrowserRouter , RouterProvider } from "react-router-dom";
+import { createBrowserRouter , RouterProvider , Outlet} from "react-router-dom";
 import About from "./components/About";
 import ContactUs from "./components/ContactUs";
 import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
 
 
 function App(){
     return (
         <div>
             <Header></Header>
-            <Body></Body>
+            <Outlet></Outlet>
         </div>
     )
 }
@@ -22,16 +23,28 @@ const appRouter = createBrowserRouter([
     {
         path : "/",
         element : <App></App>,
+        children : [
+            {
+                path : "/",
+                element : <Body></Body>,
+            },
+            {
+                path : "/about",
+                element : <About></About>,
+            },
+            {
+                path : "/contactus",
+                element : <ContactUs></ContactUs>,
+            },
+            {
+                // :resId =>  This is dynamic it can be changed according to restaurantId changed
+                path : "/restaurants/:resId",
+                element : <RestaurantMenu></RestaurantMenu>
+            }
+        ],
         errorElement : <Error></Error>
     },
-    {
-        path : "/about",
-        element : <About></About>,
-    },
-    {
-        path : "/contactus",
-        element : <ContactUs></ContactUs>
-    }
+    
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
